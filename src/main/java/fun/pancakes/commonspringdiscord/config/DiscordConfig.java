86e22f8e52c89eccf12e47aee7bc9c6c6ac6a1a8
alex-stewart <imalexstewart+git@gmail.com>
2023-11-18 09:48:17 +0000

    adding component scan

--- src/main/java/fun/pancakes/commonspringdiscord/config/DiscordConfig.java
+++ src/main/java/fun/pancakes/commonspringdiscord/config/DiscordConfig.java
@@ -4,9 +4,11 @@ import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("fun.pancakes.commonspringdiscord.*")
 public class DiscordConfig {
 
     @Bean
