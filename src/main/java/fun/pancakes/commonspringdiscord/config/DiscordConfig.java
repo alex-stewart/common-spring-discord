e6773f7c690d2e7cc8519ba6f7f3494e416b96e1
alex-stewart <imalexstewart+git@gmail.com>
2023-12-02 10:22:04 +0000

    adding debug log with discord bot token

--- src/main/java/fun/pancakes/commonspringdiscord/config/DiscordConfig.java
+++ src/main/java/fun/pancakes/commonspringdiscord/config/DiscordConfig.java
@@ -1,5 +1,6 @@
 package fun.pancakes.commonspringdiscord.config;
 
+import lombok.extern.log4j.Log4j2;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,12 +8,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@Log4j2
 @Configuration
 @ComponentScan("fun.pancakes.commonspringdiscord.*")
 public class DiscordConfig {
 
     @Bean
     public DiscordApi discordApi(@Value("${discord.bot.token}") String discordBotToken) {
+        log.debug("Authenticating discord bot with token: {}", discordBotToken);
         return new DiscordApiBuilder()
                 .setToken(discordBotToken)
                 .login().join();
