5e65e31d4e5c4e22a5737797fee6de8ea870ce22
alex-stewart <imalexstewart+git@gmail.com>
2023-12-02 14:52:32 +0000

    log text observations

--- src/main/java/fun/pancakes/commonspringdiscord/config/ObservationConfig.java
+++ src/main/java/fun/pancakes/commonspringdiscord/config/ObservationConfig.java
@@ -1,6 +1,9 @@
 package fun.pancakes.commonspringdiscord.config;
 
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationHandler;
 import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.ObservationTextPublisher;
 import io.micrometer.observation.aop.ObservedAspect;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +16,11 @@ public class ObservationConfig {
     @Bean
     public ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
         return new ObservedAspect(observationRegistry);
+    }
+
+    @Bean
+    public ObservationHandler<Observation.Context> observationTextPublisher() {
+        return new ObservationTextPublisher(log::info);
     }
 
 }
